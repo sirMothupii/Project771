ba7921e8af69e39acdfa63c13f2e5d25819df548
sirMothupii <iamkeenan94@gmail.com>
2017-09-09 21:19:08 +0200

    offline library content, with upload and download

--- app/src/main/java/com/example/mothupii_/project77/OfflineLibrary.java
+++ app/src/main/java/com/example/mothupii_/project77/OfflineLibrary.java
@@ -1,13 +1,23 @@
 package com.example.mothupii_.project77;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class OfflineLibrary extends AppCompatActivity {
+public class OfflineLibrary extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline_library);
+    }
+
+    public void onClick(View view)
+    {
+        Intent intent1 = new Intent(OfflineLibrary.this, OfflineLibraryContent.class);
+        startActivity(intent1);
     }
 }
