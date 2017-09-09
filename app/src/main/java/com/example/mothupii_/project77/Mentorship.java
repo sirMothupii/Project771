1342b797d65599078b33179d495963bbd386d5b9
Sponch <siyammukela@gmail.com>
2017-09-09 21:18:11 +0200

    Mentorship Activity Added

---
+++ app/src/main/java/com/example/mothupii_/project77/Mentorship.java
@@ -0,0 +1,13 @@
+package com.example.mothupii_.project77;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mentorship extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Mentorship);
+    }
+}
